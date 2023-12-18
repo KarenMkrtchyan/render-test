bd947ad725438a8b3fd25ec8e57f65e52fcc3954
KarenMkrtchyan <70455623+KarenMkrtchyan@users.noreply.github.com>
2023-12-17 22:17:51 -0800

    frontend integration

---
+++ notesFrontend/src/components/Note.jsx
@@ -0,0 +1,10 @@
+const Note = ({ note, toggleImportance }) => {
+	const label = note.important ? "mark not important" : "make important";
+	return (
+	<li className="note">
+		{note.content}
+		<button onClick={toggleImportance}>{label}</button>
+	</li>);
+};
+
+export default Note;
